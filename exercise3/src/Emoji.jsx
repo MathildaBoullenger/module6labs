553cd61679be4134b4986d4c8ddfc5e7ea45ad68
Mathilda <mathilda.boullenger@outlook.com>
2023-06-06 22:51:06 +1200

    Submission of exercises 1, 2 and 3 of module6

---
+++ exercise3/src/Emoji.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import happyEmoji from './assets/happyEmoji.png'
+import sadEmoji from './assets/sadEmoji.png'
+
+function Emoji() {
+   const [mood, setMood] = useState(happyEmoji)
+
+   const changeEmoji = () => {
+    setMood(mood === happyEmoji ? sadEmoji : happyEmoji);
+    }
+  
+
+   return (
+   <div>
+   <button onClick={changeEmoji}>Switch emoji</button>
+   <img src={mood} className="Happy Emoji" alt="Happy Emoji" height="300"/>
+   
+   </div>
+   );
+
+ }
+
+export default Emoji;
